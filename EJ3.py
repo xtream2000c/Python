87b2fe879f2db49fa05979624ccb39367105ea66
xtream2000c <carlmarcast@gmail.com>
2021-11-02 19:20:04 +0100

    EJ3 completado

---
+++ EJ3.py
@@ -0,0 +1,9 @@
+#Funcion que recorra una cadena
+
+cadena = input("Intruduzca cadena: ")
+
+contador = 0
+
+for caracter in cadena:
+    contador +=1
+print("hay un total de: ", contador, " caracteres.")
