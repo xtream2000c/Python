1a72dc292bc49fcae4956543673060341dbf5b7d
xtream2000c <carlmarcast@gmail.com>
2021-11-02 19:28:13 +0100

    EJ4 completado

---
+++ EJ4.py
@@ -0,0 +1,16 @@
+#comprobar si es vocal.
+
+letra = input("Introduzca un caracter: ")
+
+if letra == 'a' or letra == 'A':
+    print("Es vocal.")
+elif letra == 'e' or letra == 'E':
+    print("Es vocal.")
+elif letra == 'i' or letra == 'I':
+    print("Es vocal.")
+elif letra == 'o' or letra == 'O':
+    print("Es vocal.")
+elif letra == 'u' or letra == 'U':
+    print("Es vocal.")
+else:
+    print("No es vocal.")
